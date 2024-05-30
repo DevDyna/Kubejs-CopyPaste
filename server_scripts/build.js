767aa50101ee343677a82094a4fcebc0b9634ca1
DevDyna <78147916+DevDyna@users.noreply.github.com>
2024-05-30 18:19:36 +0200

    added offset

--- server_scripts/build.js
+++ server_scripts/build.js
@@ -1,56 +1,75 @@
-BlockEvents.rightClicked("minecraft:bedrock", (event) => {
-  const { block, level, server, player ,hand} = event;
-  const { x, y, z } = block;
-  const onhand = player.mainHandItem.id;
+function paste(item, name, speed, x_offset, y_offset, z_offset) {
+  BlockEvents.rightClicked("minecraft:bedrock", (event) => {
+    const { block, level, server, player, hand } = event;
+    const { x, y, z } = block;
+    const onhand = player.mainHandItem.id;
 
-  if (hand == "OFF_HAND") return;
+    if (hand == "OFF_HAND") return;
 
-  //try{
-  let url = "kubejs/_schematics/" + "3x3x3_1707390258622_schematic" + ".json"; //folder + uuid + extension
-  let base = JsonIO.read(url);
-  //console.log(JsonIO.read(url).structure)
+    //try{
+    let url = "kubejs/_schematics/" + name + ".json"; //folder + uuid + extension
+    let base = JsonIO.read(url);
+    //console.log(JsonIO.read(url).structure)
 
-  if (onhand == "minecraft:stick" && hand == "MAIN_HAND") {
-    //console.printObject(base.structure)
-    //[block.id]*3 -> [block.id]*2
+    if (onhand == item && hand == "MAIN_HAND") {
+      //console.printObject(base.structure)
+      //[block.id]*3 -> [block.id]*2
 
-	let indexX = 0
-	let indexY = 0
-	let indexZ = 0
+      let indexX = 0;
+      let indexY = 0;
+      let indexZ = 0;
 
-	let obj = {
-		coord: {
-			x: [],
-			y: [],
-			z: []
-		},
-		id: []
-	}
+      let obj = {
+        coord: {
+          x: [],
+          y: [],
+          z: [],
+        },
+        id: [],
+      };
 
+      base.structure.forEach((element) => {
+        element.forEach((data) => {
+          data.forEach((omega) => {
+            obj.id.push(omega);
+            obj.coord.x.push(x + indexX + x_offset);
+            obj.coord.y.push(y + indexY + y_offset);
+            obj.coord.z.push(z + indexZ + z_offset);
 
-    base.structure.forEach((element) => {
-      element.forEach((data) => {
-        data.forEach((omega) => {
-
-			obj.id.push(omega)
-			obj.coord.x.push(x+ indexX+1)
-			obj.coord.y.push(y+ indexY+1)
-			obj.coord.z.push(z+ indexZ+1)
-
-			indexZ++
+            indexZ++;
+          });
+          indexZ = 0;
+          indexY++;
         });
-		indexZ=0
-		indexY++
+        indexY = 0;
+        indexX++;
       });
-	  indexY=0
-	  indexX++
-	});
 
-	obj.id.forEach((item,index)=>{
-		server.scheduleInTicks(5*index,a=>{
-			level.getBlock(obj.coord.x[index],obj.coord.y[index],obj.coord.z[index]).set(obj.id[index])
-			server.runCommandSilent(`execute at ${player.uuid} run particle dust 0.000 1.500 0.000 1 ${ obj.coord.x[index] + 1} ${obj.coord.y[index] + 1} ${obj.coord.z[index] + 1} 0 0 0 0.1 0 normal`);
-		})
-	})
-  }
-});
+      let inertia = 0;
+      obj.id.forEach((item, index) => {
+        if (obj.id[index] == "minecraft:air") {
+          inertia++;
+        } else {
+          server.scheduleInTicks(speed * (index - 0), (a) => {
+            level
+              .getBlock(
+                obj.coord.x[index],
+                obj.coord.y[index],
+                obj.coord.z[index]
+              )
+              .set(obj.id[index]);
+            server.runCommandSilent(
+              `execute at ${
+                player.uuid
+              } run particle dust 0.000 1.500 0.000 1 ${
+                obj.coord.x[index] + 1
+              } ${obj.coord.y[index] + 1} ${
+                obj.coord.z[index] + 1
+              } 0 0 0 0.1 0 normal`
+            );
+          });
+        }
+      });
+    }
+  });
+}
